c45bcdd73131970ea611cacd9e5430ddad700005
Tekalign Mesfin <tekahazi06@gmail.com>
2024-12-14 01:20:00 +0300

    create a posts servicies

--- posts/index.js
+++ posts/index.js
@@ -1,29 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors(
+    {
+    origin: 'http://localhost:5173'
+    }
+));
 
 const PORT = 3000;
 
-const posts = {
+const posts = [{
     id: '123',
+    data: {
     title: 'My first post',
     content: 'This is my first post'
-}
+    }
+}];
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+    res.status(200).send(posts);
     }
 );
 
 app.post('/posts', (req, res) => {
     const id = randomBytes(4).toString('hex');
-    const newPost = req.body;
-    newPost.id = id;
+    const data = req.body;
+
+    const newPost = {
+        id,
+        data
+    };
     posts.push(newPost);
-    res.send(newPost);
+    res.status(201).send(newPost);
     }
 );
 
