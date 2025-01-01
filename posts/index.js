c6a25164d444ad884245e733d95b4361846b5c94
Tekalign Mesfin <tekahazi06@gmail.com>
2025-01-01 22:53:00 +0300

    Refactor posts endpoint to improve code consistency and add response for events

--- posts/index.js
+++ posts/index.js
@@ -1,58 +1,56 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {randomBytes} = require('crypto');
-const axios = require('axios')
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors(
     {
-    origin: 'http://localhost:5173',
-    credentials: true
+        origin: "http://localhost:5173",
+        credentials: true,
     }
 ));
 
 const PORT = 3000;
 
-const posts = [{
-    id: '123',
+const posts = [
+  {
+    id: "123",
     data: {
-    title: 'My first post',
-    content: 'This is my first post'
-    }
-}];
+      title: "My first post",
+      content: "This is my first post",
+    },
+  },
+];
 
-app.get('/posts', (req, res) => {
-    res.status(200).send(posts);
-    }
-);
+app.get("/posts", (req, res) => {
+  res.status(200).send(posts);
+});
 
-app.post('/posts', async (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const data = req.body;
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const data = req.body;
 
-    const newPost = {
-        id,
-        data
-    };
-    await axios.post('http://localhost:3005/events', {
-        type: 'PostCreated',
-        data: newPost
-    });
-    posts.push(newPost);
-    res.status(201).send(newPost);
-    }
-);
+  const newPost = {
+    id,
+    data,
+  };
+  await axios.post("http://localhost:3005/events", {
+    type: "PostCreated",
+    data: newPost,
+  });
+  posts.push(newPost);
+  res.status(201).send(newPost);
+});
 
-// create events end point 
-app.post('events', (req, res)=>{
-    console.log(req.body.type)
-})
+// create events end point
+app.post("/events", (req, res) => {
+  console.log(req.body.type);
+  res.send({ status: "OK" });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    }
-);
-
-
+  console.log(`Server is running on port ${PORT}`);
+});
